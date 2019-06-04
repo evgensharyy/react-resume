001b2853f61e39249156738b1cbfa0a2b615f204
evgensharyy <evgensharyy@gmail.com>
2019-06-04 16:29:00 +0300

    use makeStyles in material-ui

--- components/App/App.js
+++ components/App/App.js
@@ -4,9 +4,27 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Navbar from '../Menu/Menu';
+
+const useStyles = makeStyles({
+    card: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
 
 class App extends React.Component {
     constructor(props) {
@@ -35,7 +53,7 @@ function Content() {
     return (
         <div>
             <Container>
-                <Card>
+                <Card className={classes.card}>
                 <CardContent>
                     <Row>
                         <Col sm={8}>sm=8</Col>
